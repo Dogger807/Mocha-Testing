436ee3949d519690b17ef2ccfa414b6506f54d75
Derrick <derrick@clientcpr.com>
2017-05-21 18:03:20 -0400

    Time

---
+++ test/controllers/auth.controller.spec.js
@@ -0,0 +1,13 @@
+var assert = require('assert')
+var authController = require('../../controllers/auth.controller');
+
+describe('AuthController', function(){
+    describe('isAuthorized', function(){
+        it('should return false if not authorized', function(){
+            assert.equal(false, authController.isAuthorized(['user'], 'admin'));
+        })
+         it('should return true if authorized', function(){
+            assert.equal(true, authController.isAuthorized(['user','admin'], 'admin'));
+        })
+    })    
+});
